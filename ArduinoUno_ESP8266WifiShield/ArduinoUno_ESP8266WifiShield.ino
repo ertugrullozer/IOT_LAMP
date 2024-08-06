06536c228fde27c1dcf5f9d2439829faf430bd1b
Ertugrulozer <162031787+ertugrullozer@users.noreply.github.com>
2024-08-06 13:21:01 +0300

    Add files via upload

---
+++ ArduinoUno_ESP8266WifiShield/ArduinoUno_ESP8266WifiShield.ino
@@ -0,0 +1,54 @@
+
+#define BLYNK_TEMPLATE_ID "TMPL6zE5jjdok"
+#define BLYNK_TEMPLATE_NAME "IOT OZER HOME"
+#define BLYNK_AUTH_TOKEN "VQa85Fkkx2Ec**************"
+
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "*******";
+char pass[] = "*******";
+
+// Hardware Serial on Mega, Leonardo, Micro...
+//#define EspSerial Serial1
+
+// or Software Serial on Uno, Nano...
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 38400
+
+ESP8266 wifi(&EspSerial);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  //Blynk.begin(auth, wifi, ssid, pass);
+  // You can also specify server:
+  Blynk.begin(auth, wifi, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
